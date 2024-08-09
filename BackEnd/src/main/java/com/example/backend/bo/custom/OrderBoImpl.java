fd698fdee860b6dad9176592a90822ddc245df0e
weerapperuma <weerapperumasahiru@gmail.com>
2024-08-09 19:25:26 +0530

    file structured

---
+++ BackEnd/src/main/java/com/example/backend/bo/custom/OrderBoImpl.java
@@ -0,0 +1,102 @@
+package com.example.backend.bo.custom;
+
+import com.example.backend.dao.DAOFactory;
+import com.example.backend.dao.custom.ItemDao;
+import com.example.backend.dao.custom.OrderDao;
+import com.example.backend.dao.custom.OrderDetailDao;
+import com.example.backend.db.ConnectionManager;
+import com.example.backend.dto.ItemDto;
+import com.example.backend.dto.OrderDto;
+import com.example.backend.entity.Item;
+import com.example.backend.entity.Order;
+import com.example.backend.entity.OrderDetails;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderBoImpl implements OrderBo {
+    OrderDao orderDAO =
+            (OrderDao) DAOFactory.getInstance()
+                    .getDAO(DAOFactory.DAOType.Order);
+
+    OrderDetailDao orderItemDetailDAO =
+            (OrderDetailDao) DAOFactory.getInstance()
+                    .getDAO(DAOFactory.DAOType.OrderDetail);
+
+    ItemDao itemDao = (ItemDao) DAOFactory.getInstance().getDAO(DAOFactory.DAOType.ITEM);
+
+    @Override
+    public boolean saveOrder(OrderDto dto) throws SQLException {
+        Connection connection = ConnectionManager.getInstance().getConnection();
+        boolean isOrderSaved;
+        boolean isOrderItemSaved = true;
+        connection.setAutoCommit(false);
+
+        // Save order
+        isOrderSaved = orderDAO.save(new Order(
+                dto.getId(),
+                dto.getDate(),
+                dto.getCustomerId(),
+                Double.parseDouble(dto.getTotal()),
+                dto.getDiscount(),
+                Double.parseDouble(dto.getSubTotal()),
+                Double.parseDouble(dto.getCash()),
+                Double.parseDouble(dto.getBalance())
+        ));
+
+        // Save order items
+        if (isOrderSaved) {
+            for (ItemDto item : dto.getItems()) {
+                boolean isOrderItemDetailSaved = orderItemDetailDAO.save(new OrderDetails(
+                        dto.getId(),
+                        item.getId(),
+                        item.getQty()
+                ));
+                if (!isOrderItemDetailSaved) {
+                    connection.rollback(); // Rollback transaction on failure
+                    isOrderItemSaved = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(dto.getItems()+"get Items");
+        if (isOrderSaved){
+            for (ItemDto item : dto.getItems()) {
+                Item data = itemDao.getData(item.getId());
+                data.setQty(data.getQty() - item.getQty());
+                itemDao.update(data);
+            }
+        }
+
+        if (isOrderSaved && isOrderItemSaved) {
+            connection.commit();
+        } else {
+            connection.rollback();
+        }
+        connection.setAutoCommit(true);
+        return isOrderSaved;
+    }
+
+    @Override
+    public List<OrderDto> getAllOrders() throws SQLException {
+        List<Order> orderEntities = orderDAO.getAll();
+        List<OrderDto> orderDTOS = new ArrayList<>();
+        for (Order order : orderEntities) {
+            orderDTOS.add(
+                    new OrderDto(
+                            order.getId(),
+                            order.getDate(),
+                            order.getCustomerId(),
+                            null,
+                            String.valueOf(order.getTotal()),
+                            order.getDiscount(),
+                            String.valueOf(order.getSubTotal()),
+                            String.valueOf(order.getCash()),
+                            String.valueOf(order.getBalance())
+                    ));
+        }
+        return orderDTOS;
+    }
+}
--- FrontEnd/controller/CustomerController.js
+++ FrontEnd/controller/CustomerController.js
@@ -1,110 +1,111 @@
-import { saveCustomer } from "../model/Customer.js";
-import { getAllCustomers } from "../model/Customer.js";
-import { updateCustomer } from "../model/Customer.js";
-import { deleteCustomer } from "../model/Customer.js";
+import { saveCustomer } from '../model/CustomerModel.js';
+import { getAllCustomers } from '../model/CustomerModel.js';
+import { updateCustomer } from '../model/CustomerModel.js';
+import { deleteCustomer } from '../model/CustomerModel.js';
 
-$(document).ready(function () {
+$(document).ready(function(){
     refresh();
 });
 
-document
-    .querySelector("#CustomerManage #customerForm")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
-    });
+document.querySelector('#CustomerManage #customerForm').addEventListener('submit', function(event){
+    event.preventDefault();
+});
 
-var custId;
+var custId ;
 var custName;
 var custAddress;
 var custSalary;
 
-$("#CustomerManage .saveBtn").click(function () {
-    custId = $("#CustomerManage .custId").val();
-    custName = $("#CustomerManage .custName").val();
-    custAddress = $("#CustomerManage .custAddress").val();
-    custSalary = $("#CustomerManage .custSalary").val();
+$('#CustomerManage .saveBtn').click(function(){
+
+    custId = $('#CustomerManage .custId').val();
+    custName = $('#CustomerManage .custName').val();
+    custAddress = $('#CustomerManage .custAddress').val();
+    custSalary = $('#CustomerManage .custSalary').val();
 
     let customer = {
-        custId: custId,
-        custName: custName,
-        custAddress: custAddress,
-        custSalary: custSalary,
-    };
+        custId : custId,
+        custName : custName,
+        custAddress : custAddress,
+        custSalary : custSalary
+    }
 
     let validResult = validate(customer);
 
-    if (validResult) {
+    if(validResult){
         saveCustomer(customer);
+        alert('Customer Saved');
         refresh();
     }
+
 });
 
-function validate(customer) {
+
+async function validate(customer) {
+
     let valid = true;
 
-    if (/^C0[0-9]+$/.test(customer.custId)) {
-        $("#CustomerManage .invalidCustId").text("");
+    if ((/^C0[0-9]+$/).test(customer.custId)) {
+        $('#CustomerManage .invalidCustId').text('');
         valid = true;
     } else {
-        $("#CustomerManage .invalidCustId").text("Invalid Customer Id!");
+        $('#CustomerManage .invalidCustId').text('Invalid Customer Id');
         valid = false;
     }
 
     if ((/^(?:[A-Z][a-z]*)(?: [A-Z][a-z]*)*$/).test(customer.custName)) {
-        $("#CustomerManage .invalidCustName").text("");
+        $('#CustomerManage .invalidCustName').text('');
 
         if (valid) {
             valid = true;
         }
     } else {
-        $("#CustomerManage .invalidCustName").text("Invalid Customer Name!");
+        $('#CustomerManage .invalidCustName').text('Invalid Customer Name');
         valid = false;
     }
 
-    if (/^[A-Z][a-z, ]+$/.test(customer.custAddress)) {
-        $("#CustomerManage .invalidCustAddress").text("");
+    if ((/^[A-Z][a-z, ]+$/).test(customer.custAddress)) {
+        $('#CustomerManage .invalidCustAddress').text('');
 
         if (valid) {
             valid = true;
         }
     } else {
-        $("#CustomerManage .invalidCustAddress").text("Invalid Customer Address!");
+        $('#CustomerManage .invalidCustAddress').text('Invalid Customer Address');
         valid = false;
     }
 
     if (customer.custSalary != null && customer.custSalary > 0) {
-        $("#CustomerManage .invalidCustSalary").text("");
+        $('#CustomerManage .invalidCustSalary').text('');
         if (valid) {
             valid = true;
         }
     } else {
-        $("#CustomerManage .invalidCustSalary").text("Invalid Customer Salary!");
+        $('#CustomerManage .invalidCustSalary').text('Invalid Customer Salary');
         valid = false;
     }
 
-    let customers = getAllCustomers();
+    let customers = await getAllCustomers();
     for (let i = 0; i < customers.length; i++) {
         if (customers[i].custId === customer.custId) {
-            $("#CustomerManage .invalidCustId").text("Customer Id Already Exists!");
+            $('#CustomerManage .invalidCustId').text('Customer Id Already Exists');
             valid = false;
         }
     }
+
     return valid;
 }
 
-function createCustomerId() {
-    let customers = getAllCustomers();
-
-    if (!customers || customers.length === 0) {
-        return "C01";
-    } else {
-        let lastCustomer = customers[customers.length - 1];
-        let id = lastCustomer && lastCustomer.custId ? lastCustomer.custId : "C00";
-
-        let number = extractNumber(id);
-        number++;
-        return "C0" + number;
-    }
+function loadTable(customer){
+    console.log(customer,"=============#$$$$$$$$$========================table Load");
+    $('#CustomerManage .tableRow').append(
+        '<tr> ' +
+            '<td>' + customer.custId + '</td>' +
+            '<td>' + customer.custName + '</td>' +
+            '<td>' + customer.custAddress + '</td>' +
+            '<td>' + customer.custSalary + '</td>' +
+        '</tr>' 
+    );
 }
 
 function extractNumber(id) {
@@ -115,107 +116,139 @@ function extractNumber(id) {
     return null;
 }
 
-$("#CustomerManage .clearBtn").click(function () {
+async function createCustomerId() {
+    let customers = await getAllCustomers();
+    console.log(customers,"Enna yanna apirh");
+    if (!customers || customers.length === 0) {
+        $('#CustomerManage .custId').val("C01")
+    } else {
+        let lastCustomer = customers[customers.length - 1];
+        console.log(lastCustomer,"_-------------------------last");
+        let id = lastCustomer && lastCustomer.custId ? lastCustomer.custId : 'C00';
+        console.log(id);
+        let number = extractNumber(id);
+        number++;
+        console.log(number)
+        const nextId = 'C0' + number;
+        console.log(nextId);
+        $('#CustomerManage .custId').val(nextId)
+    }
+}
+
+async function refresh() {
+    createCustomerId()
+    $('#CustomerManage .custName').val('');
+    $('#CustomerManage .custAddress').val('');
+    $('#CustomerManage .custSalary').val('');
+    $('#CustomerManage .invalidCustId').text('');
+    $('#CustomerManage .invalidCustName').text('');
+    $('#CustomerManage .invalidCustAddress').text('');
+    $('.counts .customers h2').text(await getAllCustomers().length);
+    reloadTable();
+    // loadCustomers()
+}
+
+$('#CustomerManage .cleatBtn').click(function(){
     refresh();
 });
 
-function refresh() {
-    $("#CustomerManage .custId").val(createCustomerId());
-    $("#CustomerManage .custName").val("");
-    $("#CustomerManage .custAddress").val("");
-    $("#CustomerManage .custSalary").val("");
-    $("#CustomerManage .invalidCustId").text("");
-    $("#CustomerManage .invalidCustName").text("");
-    $("#CustomerManage .invalidCustAddress").text("");
-
-    reloadTable();
-}
-
-function reloadTable() {
-    let customers = getAllCustomers();
-    $("#CustomerManage .tableRow").empty();
-    customers.forEach((c) => {
-        loadTable(c);
-    });
-}
-
-function loadTable(customer) {
-    $("#CustomerManage .tableRow").append(
-        "<tr> " +
-        "<td>" +
-        customer.custId +
-        "</td>" +
-        "<td>" +
-        customer.custName +
-        "</td>" +
-        "<td>" +
-        customer.custAddress +
-        "</td>" +
-        "<td>" +
-        customer.custSalary +
-        "</td>" +
-        "</tr>"
-    );
-}
-
-$("#CustomerManage .searchBtn").click(function () {
-    let customer = searchCustomer($("#CustomerManage .custId").val());
-    if (customer) {
-        $("#CustomerManage .custName").val(customer.custName);
-        $("#CustomerManage .custAddress").val(customer.custAddress);
-        $("#CustomerManage .custSalary").val(customer.custSalary);
-    } else {
-        alert("Customer Not Found");
-    }
+$('#CustomerManage .searchBtn').click(function(){
+    let customer = searchCustomer($('#CustomerManage .custId').val());
 });
 
-function searchCustomer(id) {
-    let customers = getAllCustomers();
-    let customer = customers.find((c) => c.custId === id);
-    return customer;
+async function searchCustomer(id) {
+    try {
+        const customers = await getAllCustomers();
+        let customer = customers.find(c => c.custId === id);
+        console.log(customer, " =================")
+        if(customer){
+            $('#CustomerManage .custName').val(customer.custName);
+            $('#CustomerManage .custAddress').val(customer.custAddress);
+            $('#CustomerManage .custSalary').val(customer.custSalary);
+        }
+        else{
+            alert('Customer Not Found');
+        }
+        return customer;
+    } catch (error) {
+        console.error(error)
+        return null;
+    }
 }
 
-$("#CustomerManage .updateBtn").click(function () {
+$('#CustomerManage .updateBtn').click(async function () {
+
     let UpdateCustomer = {
-        custId: "C00",
-        custName: $("#CustomerManage .custName").val(),
-        custAddress: $("#CustomerManage .custAddress").val(),
-        custSalary: $("#CustomerManage .custSalary").val(),
-    };
+        custId: 'C00',
+        custName: $('#CustomerManage .custName').val(),
+        custAddress: $('#CustomerManage .custAddress').val(),
+        custSalary: $('#CustomerManage .custSalary').val()
+    }
 
     let validResult = validate(UpdateCustomer);
 
-    UpdateCustomer.custId = $("#CustomerManage .custId").val();
+    UpdateCustomer.custId = $('#CustomerManage .custId').val();
 
     if (validResult) {
-        let customers = getAllCustomers();
-        let index = customers.findIndex((c) => c.custId === UpdateCustomer.custId);
+        let customers = await getAllCustomers();
+        let index = customers.findIndex(c => c.custId === UpdateCustomer.custId);
         updateCustomer(index, UpdateCustomer);
+        alert('Customer Updated');
         refresh();
     }
+
 });
 
-$("#CustomerManage .removeBtn").click(function () {
-    let customers = getAllCustomers();
-    let id = $("#CustomerManage .custId").val();
+async function reloadTable() {
+    let customers = await getAllCustomers();
+    $('#CustomerManage .tableRow').empty()
+    console.log(customers, "=====================================================table Load");
+    // customers.forEach(c => {
+    //     loadTable(c);
+    // });
+    getAllCustomers().then((customer) => {
+        customer.forEach((customer) => {
+            loadTable(customer);
+        })
+    }).catch(
+        (error) => {
+            console.log(error);
+        }
+    )
+}
 
-    let index = customers.findIndex((c) => c.custId === id);
+$('#CustomerManage .removeBtn').click(async function () {
+    let customers = await getAllCustomers();
+    let id = $('#CustomerManage .custId').val();
+
+    let index = customers.findIndex(c => c.custId === id);
     if (index >= 0) {
-        deleteCustomer(index);
+        deleteCustomer(id);
+        alert('Customer Deleted');
         refresh();
     } else {
-        alert("Customer Not Found!");
+        alert('Customer Not Found');
     }
 });
 
-$("#CustomerManage .tableRow").on("click", "tr", function () {
-    let id = $(this).children("td:eq(0)").text();
-    let name = $(this).children("td:eq(1)").text();
-    let qty = $(this).children("td:eq(2)").text();
-    let price = $(this).children("td:eq(3)").text();
+$('#CustomerManage .tableRow').on('click', 'tr', function(){
+    let id = $(this).children('td:eq(0)').text();
+    let name = $(this).children('td:eq(1)').text();
+    let qty = $(this).children('td:eq(2)').text();
+    let price = $(this).children('td:eq(3)').text();
 
-    $("#CustomerManage .custId").val(id);
-    $("#CustomerManage .custName").val(name);
-    $("#CustomerManage .custAddress").val(qty);
-    $("#CustomerManage .custSalary").val(price);
+    $('#CustomerManage .custId').val(id);
+    $('#CustomerManage .custName').val(name);
+    $('#CustomerManage .custAddress').val(qty);
+    $('#CustomerManage .custSalary').val(price);
 });
+
+// export function loadCustomers(){
+//     getAllCustomers().then((customer) =>{
+//         console.log(customer,"=============================================getAllCustomers");
+//         customer.map((customer) => {
+//             loadTable(customer);
+//         });
+//     })
+// }
+
